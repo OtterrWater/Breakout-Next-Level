82365d3366b620c1b0f540df7a9e51af8eca303b
Jacob-Asaad <89406161+Jacob-Asaad@users.noreply.github.com>
2022-11-02 18:19:41 -0700

    Commit
    Folder reorganization
    Added a Launcher Script

---
+++ BreakoutNextLevel/Assets/Launcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Profile
+{
+    public string username;
+    public int coins;
+}
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
