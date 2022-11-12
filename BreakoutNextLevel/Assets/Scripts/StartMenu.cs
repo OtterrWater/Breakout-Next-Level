e47bb2beadc3ecb8f2108e42d34e6f30626ee3e8
OtterrWater <lee.theresa04@gmail.com>
2022-11-11 19:17:50 -0800

    Added setting Panel
    Quit game is only functional.
    
    Press cog to open cog panel, press it again to exit out of cog panel

--- BreakoutNextLevel/Assets/Scripts/StartMenu.cs
+++ BreakoutNextLevel/Assets/Scripts/StartMenu.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    public GameObject cogPanel;
     public void StartGame()
     {
         SceneManager.LoadScene("firstgame");
     }
 
+    public void openCog()
+    {
+        if (cogPanel != null)
+        {
+            bool isActive = cogPanel.activeSelf;
+            cogPanel.SetActive(!isActive);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log ("Button Quit activated");
+    }
 }
