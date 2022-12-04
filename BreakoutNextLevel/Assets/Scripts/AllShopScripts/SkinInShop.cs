3efec4ccb83ce1996992e7cdb328e08ee9c5ac7a
OtterrWater <lee.theresa04@gmail.com>
2022-12-04 15:56:58 -0800

    functional shop
    rest of the scrips has function:
    ->SkinInShop
    ->SkinLoader
    ->SkinManager
    
    connected skin to objects
    -
    >gave price and control image

--- BreakoutNextLevel/Assets/Scripts/AllShopScripts/SkinInShop.cs
+++ BreakoutNextLevel/Assets/Scripts/AllShopScripts/SkinInShop.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SkinInShop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SkinInfo skinInfo;
+    public TextMeshProUGUI buttonText;
+    public Image skinImage;
+    public bool isSkinUnlocked;
+
+    public void Awake()
     {
-        
+        skinImage.sprite = skinInfo.skinSprite;
+        IsSkinUnlocked();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IsSkinUnlocked()
     {
-        
+        if (PlayerPrefs.GetInt(skinInfo.skinID.ToString()) == 1)
+        {
+            isSkinUnlocked = true;
+            buttonText.text = "Equip";
+        }
+    }
+
+    public void OnButtonPress()
+    {
+        if (isSkinUnlocked)
+        {
+            //equip
+            FindObjectOfType<ShopMenu>().EquipSkin(skinInfo);
+        }
+        else
+        {
+            //buy
+            if (FindObjectOfType<ShopMenu>().SpendingCoins(skinInfo.skinPrice))
+            {
+                PlayerPrefs.SetInt(skinInfo.skinID.ToString(), 1);
+                IsSkinUnlocked();
+            }
+        }
     }
 }
